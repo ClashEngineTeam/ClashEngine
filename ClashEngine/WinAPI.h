32ec3270b6dc6a9a87d95d77c580b0f942c8dc38
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-05-06 15:41:50 +0300

    add WinAPI.h

---
+++ ClashEngine/WinAPI.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+//Predefined macros:https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=msvc-170
+
+#if defined(_MT) && !defined(_DLL) && !defined(_DEBUG)
+#define MT
+#elif defined(_MT) && !defined(_DLL) && defined(_DEBUG)
+#define MT_DEBUG
+#elif defined(_MT) && defined(_DLL) && !defined(_DEBUG)
+#define MD
+#elif defined(_MT) && defined(_DLL) && defined(_DEBUG)
+#define MD_DEBUG
+#endif
+
+#if defined(_DEBUG)
+#define DEBUG
+#else
+#define RELEASE
+#endif
+
+#if defined(_M_X64)
+#define X64
+#else
+#define X86
+#endif
+
+#define UNICODE
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
--- ClashEngine/main.cpp
+++ ClashEngine/main.cpp
@@ -1,9 +1,7 @@
-﻿#define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine/olcPixelGameEngine.h"
-
-#include "Random/random.hpp"
-
+﻿#include "WinAPI.h"
 #include "Kaguya/kaguya.hpp"
+#include "Random/random.hpp"
+#include "olcPixelGameEngine/olcPixelGameEngine.h"
 
 int main()
 {
