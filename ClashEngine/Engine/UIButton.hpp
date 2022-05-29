b1c2e5290d1f9eab57a9b2493df72a42ee82df9d
zmcj21 <zmcj20plusplus@gmail.com>
2022-05-29 07:33:25 +0300

    Update UIButton.hpp

--- ClashEngine/Engine/UIButton.hpp
+++ ClashEngine/Engine/UIButton.hpp
@@ -7,8 +7,47 @@ namespace ClashEngine
     class UIButton : public UIObject
     {
     public:
+        olc::Pixel backColor;
+        olc::Pixel borderColor;
+        //默认颜色:
+        olc::Pixel defaultBackColor;
+        olc::Pixel defaultBorderColor;
+        bool change_border_color = true;
+
+    public:
         UIButton(Vector2 position, Vector2 size) : UIObject(position, size)
         {
+            defaultBackColor = olc::Pixel(128, 128, 128); //Gray
+            defaultBorderColor = olc::Pixel(255, 215, 0); //Gold
+            backColor = defaultBackColor;
+            borderColor = defaultBorderColor;
+        }
+
+        void OnMouseEnter(olc::PixelGameEngine* engine) override
+        {
+            backColor = olc::Pixel(192, 192, 192); //Silver
+        }
+
+        void OnMouseStay(olc::PixelGameEngine* engine) override
+        {
+        }
+
+        void OnMouseExit(olc::PixelGameEngine* engine) override
+        {
+            backColor = defaultBackColor;
+        }
+
+        void OnMouseClick(olc::PixelGameEngine* engine) override
+        {
+            if (change_border_color)
+            {
+                borderColor = olc::Pixel(0, 139, 139); //DarkCyan
+            }
+            else
+            {
+                borderColor = defaultBorderColor;
+            }
+            change_border_color = !change_border_color;
         }
 
         void OnDraw(olc::PixelGameEngine* engine) override
@@ -18,10 +57,10 @@ namespace ClashEngine
             {
                 for (int j = 0; j < size.x; j++)
                 {
-                    engine->Draw(j + pos.x, i + pos.y, olc::Pixel(255, 255, 255));
+                    engine->Draw(j + pos.x, i + pos.y, this->backColor);
                 }
             }
-            engine->DrawRect(pos.x, pos.y, size.x, size.y, olc::Pixel(255, 0, 255));
+            engine->DrawRect(pos.x, pos.y, size.x, size.y, this->borderColor);
         }
     };
 }
