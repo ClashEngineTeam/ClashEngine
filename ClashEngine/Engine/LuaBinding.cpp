a77093f262d34806944b1135e758457fe36292ad
zmcj21 <zmcj20plusplus@gmail.com>
2022-06-06 20:29:08 +0300

    update UIInputField.

--- ClashEngine/Engine/LuaBinding.cpp
+++ ClashEngine/Engine/LuaBinding.cpp
@@ -11,6 +11,7 @@
 #include "UIButton.hpp"
 #include "UIInputField.hpp"
 #include "Vector.hpp"
+#include "EngineAPI.hpp"
 #include <string>
 #include <vector>
 #include <cmath>
@@ -245,93 +246,13 @@ namespace ClashEngine
     //基于等间隔采样的图像缩放算法:https://blog.csdn.net/qq_37394634/article/details/99675686
     static void draw_image_scaling(int x, int y, olc::Sprite* sprite, int w, int h)
     {
-        double scalingRatioX = abs((double)w / sprite->width);
-        double scalingRatioY = abs((double)h / sprite->height);
-
-        int newWidth = sprite->width * scalingRatioX;
-        int newHeight = sprite->height * scalingRatioY;
-
-        scalingRatioX = 1 / scalingRatioX;
-        scalingRatioY = 1 / scalingRatioY;
-
-        for (int i = 0; i < newHeight; i++)
-        {
-            for (int j = 0; j < newWidth; j++)
-            {
-                int xx = 0, yy = 0;
-                if (w > 0)
-                {
-                    xx = j * scalingRatioX;
-                }
-                else if (w < 0)
-                {
-                    xx = sprite->width - 1 - j * scalingRatioX;
-                }
-                if (h > 0)
-                {
-                    yy = i * scalingRatioY;
-                }
-                else if (h < 0)
-                {
-                    yy = sprite->height - 1 - i * scalingRatioY;
-                }
-                //clamp:
-                if (xx < 0) xx = 0;
-                if (yy < 0) yy = 0;
-                if (xx >= sprite->width) xx = sprite->width - 1;
-                if (yy >= sprite->height) yy = sprite->height - 1;
-                //sample:
-                olc::Pixel pixel = sprite->GetPixel(xx, yy);
-                //draw pixel:
-                LuaBinding::engine->Draw(x + j, y + i, pixel);
-            }
-        }
+        EngineAPI::DrawSprite(LuaBinding::engine, x, y, sprite, w, h);
     }
 
     //基于等间隔采样的图像缩放算法:https://blog.csdn.net/qq_37394634/article/details/99675686
     static void draw_image_scalingf(int x, int y, olc::Sprite* sprite, double sx, double sy)
     {
-        double scalingRatioX = abs(sx);
-        double scalingRatioY = abs(sy);
-
-        int newWidth = sprite->width * scalingRatioX;
-        int newHeight = sprite->height * scalingRatioY;
-
-        scalingRatioX = 1 / scalingRatioX;
-        scalingRatioY = 1 / scalingRatioY;
-
-        for (int i = 0; i < newHeight; i++)
-        {
-            for (int j = 0; j < newWidth; j++)
-            {
-                int xx = 0, yy = 0;
-                if (sx > 0)
-                {
-                    xx = j * scalingRatioX;
-                }
-                else if (sx < 0)
-                {
-                    xx = sprite->width - 1 - j * scalingRatioX;
-                }
-                if (sy > 0)
-                {
-                    yy = i * scalingRatioY;
-                }
-                else if (sy < 0)
-                {
-                    yy = sprite->height - 1 - i * scalingRatioY;
-                }
-                //clamp:
-                if (xx < 0) xx = 0;
-                if (yy < 0) yy = 0;
-                if (xx >= sprite->width) xx = sprite->width - 1;
-                if (yy >= sprite->height) yy = sprite->height - 1;
-                //sample:
-                olc::Pixel pixel = sprite->GetPixel(xx, yy);
-                //draw pixel:
-                LuaBinding::engine->Draw(x + j, y + i, pixel);
-            }
-        }
+        EngineAPI::DrawSprite(LuaBinding::engine, x, y, sprite, sx, sy);
     }
 
     //基于等间隔采样的图像缩放算法:https://blog.csdn.net/qq_37394634/article/details/99675686
@@ -733,9 +654,9 @@ namespace ClashEngine
 
     //=====================InputField APIs=====================
 
-    static UIInputField* init_input_field(int x, int y, int w, int h)
+    static UIInputField* init_input_field(int x, int y, int w, int h, olc::Font* font)
     {
-        UIInputField* inputField = new UIInputField(Vector2(x, y), Vector2(w, h));
+        UIInputField* inputField = new UIInputField(Vector2(x, y), Vector2(w, h), font);
         LuaBinding::ui_objects.push_back(inputField);
         return inputField;
     }
--- ClashEngine/Engine/UIInputField.hpp
+++ ClashEngine/Engine/UIInputField.hpp
@@ -1,20 +1,48 @@
 ﻿#pragma once
 
 #include "UIObject.hpp"
+#include "String.hpp"
+#include "StringConverter.hpp"
+#include "EngineAPI.hpp"
+#include <string>
+
+using namespace std;
 
 namespace ClashEngine
 {
+    //输入框
     class UIInputField : public UIObject
     {
     private:
-        bool flash_cursor = false;          //是否开始闪烁光标
-        const float cursor_flash_interval = 0.5f; //闪烁光标的间隔时间
-        float cursor_timer;
-        bool cursor_switch;
+        bool flash_cursor = false;                  //是否开始闪烁光标
+        const float cursor_flash_interval = 0.5f;   //闪烁光标的间隔时间
+        const int offset = 5;                       //坐标偏移
+        float cursor_timer = 0.0f;                  //计时器
+        bool cursor_switch = false;                 //光标亮灭开关
+
+    private:
+        std::wstring inputData = L"";               //输入的内容字符串
+        olc::Font* font;                            //输入框显示文字所需的字体
 
     public:
-        UIInputField(Vector2 position, Vector2 size) : UIObject(position, size)
+        UIInputField(Vector2 position, Vector2 size, olc::Font* font) : UIObject(position, size)
         {
+            this->font = font;
+        }
+
+        void OnKeyInput(olc::PixelGameEngine* engine, WCHAR c) override
+        {
+            if (c == VK_BACK)
+            {
+                if (inputData.size() > 0)
+                {
+                    inputData.pop_back();
+                }
+            }
+            else
+            {
+                inputData.push_back(c);
+            }
         }
 
         void OnMouseEnter(olc::PixelGameEngine* engine) override
@@ -48,20 +76,64 @@ namespace ClashEngine
             //draw cursor:
             if (cursor_switch && flash_cursor)
             {
-                int offset = 5;
                 olc::vi2d cursorOriginPos(pos.x + offset, pos.y + offset);
                 olc::vi2d cursorTargetPos(pos.x + offset, pos.y + offset + size.y - offset * 2);
                 engine->DrawLine(cursorOriginPos, cursorTargetPos, olc::Pixel(0, 0, 0));
             }
+            //draw string:
+            if (inputData.size() > 0)
+            {
+#ifdef UI_TEST
+                vector<FontSprite*> sprites = EngineAPI::StringToSprites(this->font, inputData, 255, 255, 255);
+                int width = 0;
+                for (auto& item : sprites)
+                {
+                    double yZoomRatio = (double)size.y / item->sprite->height;
+                    int newWidth = item->sprite->width * yZoomRatio;
+                    int newHeight = item->sprite->height * yZoomRatio;
+                    EngineAPI::DrawPNGSprite(engine, pos.x + width, pos.y, item->sprite, newWidth, newHeight);
+                    width += newWidth + (item->offset.x);
+                }
+                EngineAPI::DeleteSprites(sprites);
+#else
+                wstring sample = L"你好abAB12,./<>?-=_+`~";
+
+                std::string sampleStr = String::WstringToString(sample, Encoding::UTF8);
+                u32string u32SampleStr = StringConverter::To_UTF32(sampleStr);
+                olc::Sprite* sampleSprite = font->RenderStringToSprite(u32SampleStr, olc::BLACK);
+
+                std::string str = String::WstringToString(sample + inputData, Encoding::UTF8);
+                u32string u32str = StringConverter::To_UTF32(str);
+                olc::Sprite* fontSprite = font->RenderStringToSprite(u32str, olc::Pixel(33, 33, 33));
+
+                int diffWidth = fontSprite->width - sampleSprite->width;
+                olc::Sprite* diffSprite = new olc::Sprite(diffWidth, fontSprite->height);
+                for (int i = 0; i < diffSprite->height; i++)
+                {
+                    for (int j = 0; j < diffSprite->width; j++)
+                    {
+                        olc::Pixel p = fontSprite->GetPixel(j + sampleSprite->width, i);
+                        diffSprite->SetPixel(j, i, p);
+                    }
+                }
+
+                double yZoomRatio = (double)size.y / fontSprite->height;
+                EngineAPI::DrawPNGSprite(engine, pos.x, pos.y, diffSprite, yZoomRatio, yZoomRatio);
+
+                delete sampleSprite;
+                delete fontSprite;
+                delete diffSprite;
+#endif
+            }
         }
 
+        //该函数在OnXXX事件之前调用
         void OnUpdate(olc::PixelGameEngine* engine) override
         {
             if (engine->GetMouse(olc::Mouse::LEFT).bPressed)
             {
                 if (flash_cursor) flash_cursor = false;
             }
-
             float deltaTime = engine->GetElapsedTime();
             cursor_timer += deltaTime;
             if (cursor_timer >= cursor_flash_interval)
