f595e92b98027f7d9571f64376d0c56151562144
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-07-11 07:27:23 +0300

    add makefile

---
+++ build/make/main.cpp
@@ -0,0 +1,22 @@
+﻿#ifdef __linux__
+
+//olcPixelGameEngine:
+#define OLC_PGE_APPLICATION
+#include "../ClashEngine/olcPixelGameEngine/olcPixelGameEngine.h"
+//olcPGEX_TTF:
+//#define OLC_PGEX_TTF
+//#include "../ClashEngine/olcPGEX_TTF/olcPGEX_TTF.h"
+//format:
+#include "../ClashEngine/Engine/Format.hpp"
+//std:
+#include <iostream>
+
+int main()
+{
+    std::string s = ClashEngine::Format::format("{r}{g}{b}", 255, 0, 0);
+    std::cout << s << "\n";
+    std::cin.get();
+    return 0;
+}
+
+#endif
