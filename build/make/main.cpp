ce8afe03f58257d3445910c02491342b20b3cfae
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-07-11 07:55:42 +0300

    Update main.cpp

--- build/make/main.cpp
+++ build/make/main.cpp
@@ -8,11 +8,41 @@
 //std:
 #include <iostream>
 
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		sAppName = "Example";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// called once per frame
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand()% 256));	
+		return true;
+	}
+};
+
 int main()
 {
+    Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+
     std::string s = ClashEngine::Format::format("{r}{g}{b}", 255, 0, 0);
     std::cout << s << "\n";
     std::cin.get();
+    
     return 0;
 }
 
